7838315c13d79d18f2c3f22e8c70b32668684a69
krypton-byte <rosid6434@gmail.com>
2023-08-19 02:20:49 -0400

    refactor: split

--- lexz/__init__.py
+++ lexz/__init__.py
@@ -1,3 +1,7 @@
-from .tree_variabel import GraphGen, VarExtractor, VariableMapping
-
-__all__ = ['GraphGen', 'VarExtractor', 'VariableMapping']
+from .tree_variabel import VarExtractor
+from .LexZ import (
+    VariableMapping,
+    GraphGen,
+    Collector
+)
+__all__ = ['GraphGen', 'VarExtractor', 'VariableMapping', 'Collector']
